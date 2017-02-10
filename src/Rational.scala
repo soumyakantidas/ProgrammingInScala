e7a172e3e1720e638aed95cd796a921c2d9ef188
Soumyakanti Das <soumyakantidas007@gmail.com>
2017-02-10 16:19:27 +0530

    Added overloaded operators for +, -, * and /

--- src/Rational.scala
+++ src/Rational.scala
@@ -4,14 +4,16 @@
 class Rational(numer: Int, denom: Int) {
 	require(denom != 0, "Denominator cannot be zero!".toUpperCase)
 
-	val g = gcd(numer.abs, denom.abs)
+	private val g = gcd(numer.abs, denom.abs)
 
-	val numerator = numer/g
-	val denominator = denom/g
+	private val numerator = numer / g
+	private val denominator = denom / g
 
 	def this(numer: Int) = this(numer, 1)
 
-	override def toString: String = numerator + "/" + denominator
+	override def toString: String = {
+		if (denominator == 1) numerator.toString else numerator + "/" + denominator
+	}
 
 	def +(that: Rational): Rational = {
 		new Rational(
@@ -36,9 +38,20 @@ class Rational(numer: Int, denom: Int) {
 
 	def -(num: Int): Rational = {
 		new Rational(
-			this.numerator + this.denominator * num,
+			this.numerator - this.denominator * num,
 			this.denominator
 		)
+	}
+
+	def *(num: Int): Rational = {
+		new Rational(
+			this.numerator * num,
+			this.denominator
+		)
+	}
+
+	def /(that: Rational): Rational = {
+		this * new Rational(that.denominator, that.numerator)
 	}
 
 	def *(that: Rational): Rational = {
@@ -48,12 +61,16 @@ class Rational(numer: Int, denom: Int) {
 		)
 	}
 
-	def lessThan(that: Rational): Boolean = {
-		this.numerator * that.denominator < that.numerator * this.denominator
+	def /(num: Int): Rational = {
+		this * new Rational(1, num)
 	}
 
 	def max(that: Rational): Rational = {
 		if (this.lessThan(that)) that else this
+	}
+
+	def lessThan(that: Rational): Boolean = {
+		this.numerator * that.denominator < that.numerator * this.denominator
 	}
 
 	private def gcd(a: Int, b: Int): Int = {
